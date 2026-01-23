1c1b6df66bdb4db5ec2c591bfa954257b2ad3ec3
pmaliagil <pmalgil2102@g.educaand.es>
2026-01-23 21:08:02 +0100

    Funcionamiento Nave

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,7 +3,6 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
-#dfhviudfv
 class AlienInvasion:
     """Clase general para gestionar los recursos y el comportamiento del juego."""
 
