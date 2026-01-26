f1f4813f19477cafad6a455d4a3fd2772aa901b7
pmaliagil <pmalgil2102@g.educaand.es>
2026-01-26 18:53:00 +0100

    Añadida flota

--- alien_invasion.py
+++ alien_invasion.py
@@ -83,20 +83,26 @@ class AlienInvasion:
     def _create_fleet(self):
         """Crea la flota de aliens"""
         #Crea un alienigena y va añadiendo alienigenas hasta que no haya espacio.
-        #La distancia entre alienigenas es equivalente al ancho de un extraterrestre.
+        #El espacio entre alienigenas es de un alien de ancho y otro de largo.
         alien = Alien(self)
-        alien_width = alien.rect.width
+        alien_width, alien_height = alien.rect.size
 
-        current_x = alien_width
-        while current_x < (self.settings.screen_width - 2 * alien_width):
-            self._create_alien(current_x)
-            current_x +=2 * alien_width
-            
-    def _create_alien(self, x_position):
-        """Crea un alienigena y lo coloca en la fila"""
+        current_x, current_y = alien_width, alien_height
+        while current_y < (self.settings.screen_height - 3 * alien_height):
+            while current_x < (self.settings.screen_width - 2 * alien_width):
+                self._create_alien(current_x, current_y)
+                current_x +=2 * alien_width
+
+            #Fila terminada; resetea valor de x e incremente valor de y.
+            current_x = alien_width
+            current_y += 2 * alien_height
+                
+    def _create_alien(self, x_position, y_position):
+        """Crea un alienigena y lo coloca en la flota"""
         new_alien = Alien(self)
         new_alien.x = x_position
         new_alien.rect.x = x_position
+        new_alien.rect.y = y_position
         self.aliens.add(new_alien)
 
 
